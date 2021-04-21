a34b4733d3d827a946519c328eb64e729428a9b7
joli <1040123159@qq.com>
2021-04-21 16:00:47 +0800

    '接口封装'

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,34 @@
           <div class="main"></div>
           <div class="form">
             <h3 @click="showRegister">创建账户</h3>
-            <div v-show="isShowRegister" class="register">
-              <input type="text" v-model="register.username" placeholder="用户名" />
-              <input type="password" v-model="register.password" placeholder="密码" />
-              <p v-bind:class="{error: register.isError}">{{register.notice}}</p>
-              <div class="button" @click="onRegister">创建账号</div>　
-            </div>
+            <transition>
+              <div v-bind:class="{show: isShowRegister}" class="register">
+                <input type="text" v-model="register.username" placeholder="用户名" />
+                <input
+                  type="password"
+                  v-model="register.password"
+                  @keyup.enter="onRegister"
+                  placeholder="密码"
+                />
+                <p v-bind:class="{error: register.isError}">{{register.notice}}</p>
+                <div class="button" @click="onRegister">创建账号</div>
+              </div>
+            </transition>
+
             <h3 @click="showLogin">登录</h3>
-            <div v-show="isShowLogin" class="login">
-              <input type="text" v-model="login.username" placeholder="输入用户名" />
-              <input type="password" v-model="login.password" placeholder="密码" />
-              <p v-bind:class="{error: login.isError}">{{login.notice}}</p>
-              <div class="button" @click="onLogin">登录</div>
-            </div>
+            <transition name="slide">
+              <div v-bind:class="{show: isShowLogin}" class="login">
+                <input type="text" v-model="login.username" placeholder="输入用户名" />
+                <input
+                  type="password"
+                  v-model="login.password"
+                  @keyup.enter="onLogin"
+                  placeholder="密码"
+                />
+                <p v-bind:class="{error: login.isError}">{{login.notice}}</p>
+                <div class="button" @click="onLogin">登录</div>
+              </div>
+            </transition>
           </div>
         </div>
       </div>
@@ -27,6 +42,10 @@
 </template>
 
 <script>
+import Auth from "@/apis/auth";
+Auth.getInfo().then(data=>{
+  console.log(data)
+})
 export default {
   data() {
     return {
@@ -68,9 +87,10 @@ export default {
       }
       this.register.isError = false;
       this.register.notice = "";
-      console.log(
-        `start register..., username: ${this.register.username} , password: ${this.register.password}`
-      );
+      Auth.register({username:this.register.username,password:this.register.password})
+      .then(data=>{
+        console.log(data)
+      })
     },
     onLogin() {
       if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
@@ -85,10 +105,9 @@ export default {
       }
       this.login.isError = false;
       this.login.notice = "";
-
-      console.log(
-        `start login..., username: ${this.login.username} , password: ${this.login.password}`
-      );
+      Auth.login({username:this.login.username,password:this.login.password}).then(data=>{
+        console.log(data)
+      })
     },
   },
 };
@@ -160,8 +179,15 @@ export default {
 
     .login,
     .register {
-      padding: 10px 20px;
+      padding: 0px 20px;
       border-top: 1px solid #eee;
+      height: 0;
+      overflow: hidden;
+      transition: height 0.4s;
+
+      &.show {
+        height: 193px;
+      }
 
       input {
         display: block;
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="sidebar">
-    <Avatar/>
+    <Avatar />
     <div class="icons">
-      <router-link to="/note/1" title="笔记"><i class="iconfont icon-note"></i></router-link>
-      <router-link to="/notebooks" title="笔记本"><i class="iconfont icon-notebook"></i></router-link>
-      <router-link to="/trash/2" title="回收站"><i class="iconfont icon-trash"></i></router-link> 
-    </div> 
+      <router-link to="/note/1" title="笔记">
+        <i class="iconfont icon-note"></i>
+      </router-link>
+      <router-link to="/notebooks" title="笔记本">
+        <i class="iconfont icon-notebook"></i>
+      </router-link>
+      <router-link to="/trash/2" title="回收站">
+        <i class="iconfont icon-trash"></i>
+      </router-link>
+    </div>
     <div class="logout" @click="onLogout">
       <i class="iconfont icon-logout"></i>
     </div>
@@ -13,17 +19,23 @@
 </template>
 
 <script>
-import Avatar from './Avatar'
-  export default {
-    components:{
-      Avatar
-    }
-  }  
-
+import Avatar from "./Avatar";
+import Auth from "@/apis/auth";
+export default {
+  components: {
+    Avatar,
+  },
+  methods: {
+    onLogout() {
+      Auth.logout().then((data) => {
+        console.log(data);
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 #sidebar {
   position: relative;
   width: 56px;
